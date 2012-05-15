5870f2bb342729e21eb7c732c8795572b705062d
Ludovic Vannoorenberghe <ludovic.v@me.com>
2012-05-15 14:14:01 +0200

    Fichier example

---
+++ extract.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  SINF1252_P3
+//
+//  Created by Ludovic Vannoorenberghe on 15/05/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main (int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
---
+++ extract.h
@@ -0,0 +1,14 @@
+//
+//  extract.h
+//  SINF1252_P3
+//
+//  Created by Ludovic Vannoorenberghe on 15/05/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#ifndef SINF1252_P3_extract_h
+#define SINF1252_P3_extract_h
+
+
+
+#endif
