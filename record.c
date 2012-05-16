b91096d732ce16ceec572a08309a012c906203c8
Joey M <joey@sinf.be>
2012-05-16 17:03:38 +0200

    implemented: opening a file and showing its metadata

--- record.c
+++ record.c
@@ -10,20 +10,55 @@
 #include <signal.h>
 #include <unistd.h>
 #include <string.h>
+#include <sys/stat.h>
+#include <time.h>
+
+int timer;							// time between each file change check
+char *filepath;					// file to monitor
+struct stat metadata;		// metadata of the file to monitor
 
 static void sig_handler(int signum) {
-	char *msg = "Test\n";
-  write(STDOUT_FILENO, msg, strlen(msg));
+	if (signum == SIGALRM) {
+		char *msg = "Test\n";
+		write(STDOUT_FILENO, msg, strlen(msg));
+		alarm(3); // reset the timer
+	}
+	else if (signum == SIGINT) {
+		char *msg = "Closing program\n";
+		write(STDOUT_FILENO, msg, strlen(msg));
+		exit(EXIT_SUCCESS);
+	}
 }
 
-int main(int argc, char *argv[]) {
+void start() {
+	printf("%s\n", filepath);
+	if (stat(filepath, &metadata) == -1) {
+		perror("Error while retrieving metadata of file.");
+		exit(EXIT_FAILURE);
+	}
+	printf("Last modif: %s", ctime(&metadata.st_mtime));
+}
 
-	if (signal(SIGALRM, sig_handler) == SIG_ERR) {
-    perror("Error while registering callback function.");
+int main(int argc, char *argv[]) {	
+	
+	timer = 3; // TODO change this value to the param value
+	filepath = argv[1]; // TODO change this value to the param value
+
+	start(); // initiate the startup process
+
+	/* register the different signal handlers */
+	if (signal(SIGALRM, sig_handler) == SIG_ERR) {						// alarm handler
+    perror("Error while registering SIGALRM callback function.");
+    exit(EXIT_FAILURE);
+  }
+	if (signal(SIGINT, sig_handler) == SIG_ERR) {							// CTRL-C handler
+    perror("Error while registering SIGINT callback function.");
     exit(EXIT_FAILURE);
   }
 
-	alarm(3);
+	alarm(timer);
+
+	while (1); // run the program until a signal is sent
 
 	exit(EXIT_SUCCESS);
 }
