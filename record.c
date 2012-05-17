e6c81ee0e665ca8b117f2689ccb717dafe7fe257
Joey M <joey@sinf.be>
2012-05-17 13:43:24 +0200

    new approach (start)

--- record.c
+++ record.c
@@ -12,15 +12,33 @@
 #include <string.h>
 #include <sys/stat.h>
 #include <time.h>
+#include <errno.h>
 
 int timer;							// time between each file change check
 char *filepath;					// file to monitor
-struct stat metadata;		// metadata of the file to monitor
+struct stat mdata;			// metadata of the file to monitor
+struct stat prvmdata;		// previous metadata of the file to monitor
+struct stat archmdata;	// metadata of the archive
+FILE *archive;					// the archive
+char *archivepath;			// archive containing the versions
 
 static void sig_handler(int signum) {
 	if (signum == SIGALRM) {
-		char *msg = "Test\n";
+		char *msg = "Alarm\n";
 		write(STDOUT_FILENO, msg, strlen(msg));
+	
+		prvmdata = mdata; // saving the metadata
+		
+		if (stat(filepath, &mdata) == -1) {
+			perror("Error while retrieving metadata of monitored file.");
+			exit(EXIT_FAILURE);
+		}
+
+		if (mdata.st_mtime == prvmdata.st_mtime)
+			printf("No modification.\n");
+		else
+			printf("Modification detected!\n");
+
 		alarm(3); // reset the timer
 	}
 	else if (signum == SIGINT) {
@@ -30,28 +48,49 @@ static void sig_handler(int signum) {
 	}
 }
 
+void record() {
+	printf("Recording...\n");
+}
+
 void start() {
 	printf("%s\n", filepath);
-	if (stat(filepath, &metadata) == -1) {
-		perror("Error while retrieving metadata of file.");
+	printf("%s\n", archivepath);
+	
+	/* get the metadata of the monitored file */
+	if (stat(filepath, &mdata) == -1) {
+		perror("Error while retrieving metadata of monitored file.");
 		exit(EXIT_FAILURE);
 	}
-	printf("Last modif: %s", ctime(&metadata.st_mtime));
+
+	archive = fopen(archivepath, "a"); // opening the archive in write at end of text file mode
+
+	if (stat(archivepath, &archmdata) == -1) {
+		perror("Error while retrieving metadata of the archive.");
+		exit(EXIT_FAILURE);
+	}
+
+	if (archmdata.st_mtime != mdata.st_mtime) {
+		printf("A modification was detected since last record.\n");
+		record();
+	}
+
+	printf("Last modif: %s", ctime(&mdata.st_mtime));
 }
 
 int main(int argc, char *argv[]) {	
 	
 	timer = 3; // TODO change this value to the param value
 	filepath = argv[1]; // TODO change this value to the param value
+	archivepath = argv[2]; // TODO change this value to the param value
 
 	start(); // initiate the startup process
 
 	/* register the different signal handlers */
-	if (signal(SIGALRM, sig_handler) == SIG_ERR) {						// alarm handler
+	if (signal(SIGALRM, sig_handler) == SIG_ERR) {	// alarm handler
     perror("Error while registering SIGALRM callback function.");
     exit(EXIT_FAILURE);
   }
-	if (signal(SIGINT, sig_handler) == SIG_ERR) {							// CTRL-C handler
+	if (signal(SIGINT, sig_handler) == SIG_ERR) {		// CTRL-C handler
     perror("Error while registering SIGINT callback function.");
     exit(EXIT_FAILURE);
   }
